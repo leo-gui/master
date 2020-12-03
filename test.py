262c9bbeac51ee2d9347aa14a3a5bb24f351c1f9
桂扬 <guiyang@guiyangdeMacBook-Pro.local>
2020-12-03 20:21:19 +0800

    [Feature]add mq

--- test.py
+++ test.py
@@ -1,9 +1,13 @@
-from flask import  Flask
+from flask import Flask
+import pika
+
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return "<h1>ok<h1>"
+
+connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
 
 if __name__ == "__main__":
     app.run()
