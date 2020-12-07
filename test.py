a9f1ecc4eb0fa1c638d1537b70659b148009dbe5
桂扬 <guiyang@guiyangdeMacBook-Pro.local>
2020-12-07 11:00:58 +0800

    [Test]rabbitMQ consum

--- test.py
+++ test.py
@@ -16,5 +16,22 @@ channel.qeue_declare(queue='request')
 channel.basic_publish(exchange='', routing_key='hello', body='Hello World!')
 channel.close()
 
+connection = pika.BlockingConnection(
+    pika.ConnectionParameters(host='localhost'))
+channel = connection.channel()
+
+channel.queue_declare(queue='hello')
+
+
+def callback(ch, method, properties, body):
+    print(" [x] Received %r" % body)
+
+
+channel.basic_consume(
+    queue='hello', on_message_callback=callback, auto_ack=True)
+
+print(' [*] Waiting for messages. To exit press CTRL+C')
+channel.start_consuming()
+
 if __name__ == "__main__":
     app.run()
