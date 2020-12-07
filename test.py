ef72da4d78ec4df6d602c9a6adfd7b1c642590aa
桂扬 <guiyang@guiyangdeMacBook-Pro.local>
2020-12-07 10:23:55 +0800

    [Feature]rabbitmq

--- test.py
+++ test.py
@@ -3,11 +3,16 @@ import pika
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
-    return "<h1>ok<h1>"
+    return '<h1>success<h1>'
 
 connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
+channel = connection.channel()
+channel.qeue_declare(queue='request')
+channel.basic_publish(exchange='', routing_key='hello', body='Hello World!')
+channel.close()
 
 if __name__ == "__main__":
     app.run()
