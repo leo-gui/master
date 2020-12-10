d02d724704687833499c2f1d9ac549177d16c50d
桂扬 <guiyang@guiyangdeMacBook-Pro.local>
2020-12-10 14:43:48 +0800

    [Feature]Flsk

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
-from flask import Flask
+from flask import Flask,jsonify
 import pika
-
+from flask import request
 app = Flask(__name__)
 
 
@@ -8,28 +8,35 @@ app = Flask(__name__)
 def index():
     return '<h1>success<h1>'
 
-connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
-channel = connection.channel()
-channel.qeue_declare(queue='request')
-channel.basic_publish(exchange='', routing_key='hello', body='Hello World!')
-channel.close()
-
-connection = pika.BlockingConnection(
-    pika.ConnectionParameters(host='localhost'))
-channel = connection.channel()
-
-channel.queue_declare(queue='hello')
-
-
-def callback(ch, method, properties, body):
-    print(" [x] Received %r" % body)
-
-
-channel.basic_consume(
-    queue='hello', on_message_callback=callback, auto_ack=True)
-
-print(' [*] Waiting for messages. To exit press CTRL+C')
-channel.start_consuming()
+@app.route('/method',methods=['POST','GET'])
+def getMtehod():
+    response_list = {
+        "url":request.url,
+        "method":request.method
+    }
+    return jsonify(response_list)
+# connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
+# channel = connection.channel()
+# channel.qeue_declare(queue='request')
+# channel.basic_publish(exchange='', routing_key='hello', body='Hello World!')
+# channel.close()
+#
+# connection = pika.BlockingConnection(
+#     pika.ConnectionParameters(host='localhost'))
+# channel = connection.channel()
+#
+# channel.queue_declare(queue='hello')
+#
+#
+# def callback(ch, method, properties, body):
+#     print(" [x] Received %r" % body)
+#
+#
+# channel.basic_consume(
+#     queue='hello', on_message_callback=callback, auto_ack=True)
+#
+# print(' [*] Waiting for messages. To exit press CTRL+C')
+# channel.start_consuming()
 
 if __name__ == "__main__":
     app.run()
