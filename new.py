ac7d5480afaecbd8e374eb67f6918a23f8ec409c
桂扬 <guiyang@guiyangdeMacBook-Pro.local>
2020-12-20 13:38:18 +0800

    [Feature] flask

--- new.py
+++ new.py
@@ -1,3 +1,24 @@
-'''
-test
-'''
+class People(object):
+    def show_power(self):
+        print("i can walk")
+
+
+class Singer(People):
+    def show_power(self):
+        super(Singer, self).show_power()
+        print("i can sing")
+
+
+class Actor(People):
+    def show_power(self):
+        super(Actor, self).show_power()
+        print("i can play")
+
+
+class Human(Singer, Actor):
+    pass
+
+
+if __name__ == "__main__":
+    a = Human()
+    a.show_power()
--- test.py
+++ test.py
@@ -1,20 +1,47 @@
-from flask import Flask,jsonify
-import pika
-from flask import request
+from flask import Flask
+from flask import request,redirect,url_for
+from werkzeug.utils import secure_filename
+from flask import send_from_directory
+from pypinyin import lazy_pinyin
+import os
 app = Flask(__name__)
+UPLOAD_FOLDER = "/Users/guiyang/Documents/test/"
+ALLOW_EXTENTION = ['txt','jpg','pdf','png','doc','docx']
+
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 
-@app.route('/')
-def index():
-    return '<h1>success<h1>'
+def allow_filename(filename):
+    return "." in filename \
+            and filename.split('.')[1] in ALLOW_EXTENTION
 
-@app.route('/method',methods=['POST','GET'])
-def getMtehod():
-    response_list = {
-        "url":request.url,
-        "method":request.method
-    }
-    return jsonify(response_list)
+
+@app.route('/upload/file',methods=['POST','GET'])
+def upload_file():
+    if request.method == "POST":
+        file = request.files['file']
+        if file and allow_filename(file.filename):
+            new_filename = '_'.join(lazy_pinyin(file.filename.split('.')[0]))
+            filename = secure_filename(new_filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for("uploaded_file",filename=filename))
+        else:
+            return "<h1>该文件类型不支持</h1>"
+    return '''
+        <!doctype html>
+    <title>Upload new File</title>
+    <h1>Upload new File</h1>
+    <form action="" method=post enctype=multipart/form-data>
+      <p><input type=file name=file>
+         <input type=submit value=Upload>
+    </form>
+           '''
+
+
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'],filename)
+
 
 if __name__ == "__main__":
-    app.run()
+    app.run(port=8080)
