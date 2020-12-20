483a63e7a07ae47ea0dbb38abcef9cece4655fb5
桂扬 <guiyang@guiyangdeMacBook-Pro.local>
2020-12-20 13:45:08 +0800

    [Fix]change filename

--- upload.py
+++ upload.py
@@ -1,12 +1,12 @@
 from flask import Flask
-from flask import request,redirect,url_for
+from flask import request, redirect, url_for
 from werkzeug.utils import secure_filename
 from flask import send_from_directory
 from pypinyin import lazy_pinyin
 import os
 app = Flask(__name__)
 UPLOAD_FOLDER = "/Users/guiyang/Documents/test/"
-ALLOW_EXTENTION = ['txt','jpg','pdf','png','doc','docx']
+ALLOW_EXTENTION = ['txt', 'jpg', 'pdf', 'png', 'doc', 'docx']
 
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
@@ -16,7 +16,7 @@ def allow_filename(filename):
             and filename.split('.')[1] in ALLOW_EXTENTION
 
 
-@app.route('/upload/file',methods=['POST','GET'])
+@app.route('/upload/file', methods=['POST', 'GET'])
 def upload_file():
     if request.method == "POST":
         file = request.files['file']
@@ -24,7 +24,7 @@ def upload_file():
             new_filename = '_'.join(lazy_pinyin(file.filename.split('.')[0]))
             filename = secure_filename(new_filename)
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            return redirect(url_for("uploaded_file",filename=filename))
+            return redirect(url_for("uploaded_file", filename=filename))
         else:
             return "<h1>该文件类型不支持</h1>"
     return '''
@@ -40,7 +40,7 @@ def upload_file():
 
 @app.route('/uploads/<filename>')
 def uploaded_file(filename):
-    return send_from_directory(app.config['UPLOAD_FOLDER'],filename)
+    return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
 
 
 if __name__ == "__main__":
